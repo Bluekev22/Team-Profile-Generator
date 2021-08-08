354d2d40d73dd9ea7a3b76c7b24fed2db446f51b
Kevin Shank <kevin_shank@yahoo.com>
2021-08-08 15:57:31 -0400

    Added questions to index

--- index.js
+++ index.js
@@ -1,33 +1,157 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+const generatedHTMLPath = "./dist/TeamProfile.html";
+
+let teamMembers = [];
 
 const writeFileAsync = util.promisify(fs.writeFile);
-// name, employee ID, email address, and office number
+
 function managerQuestions() {
     return inquirer.prompt([
       {
         type: 'input',
-        name: 'name',
-        message: 'What is the title of the project?',
+        name: 'managerName',
+        message: "What is the team manager's name?",
       },
       {
         type: 'input',
-        name: 'id',
-        message: 'What is the project about? Please give a description.',
+        name: 'managerID',
+        message: "What is the team manager's ID number?",
       },
       {
         type: 'input',
-        name: 'email',
-        message: 'What does the user need to install to run this application?',
+        name: 'managerEmail',
+        message: "What is the team manager's email address?",
       },
       {
         type: 'input',
-        name: 'officeNumber',
-        message: 'How is the app used? Please give instructions',
+        name: 'managerOfficeNumber',
+        message: "What is the team manager's office number?",
       },
+      {
+        type: 'list',
+        name: "addTeamMember",
+        message: "Select team members to add",
+        choices: ["Engineer", "Intern"],
+      }
      
-    ]);
+    ])
+    .then(answers => {
+      let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+      teamMembers.push(manager);
+      if (answers.addTeamMember === "Intern") {
+          internQuestions();
+      } if (answers.addTeamMember === "Engineer") {
+          EngineerQuestions();
+      }
+    })
+
+    .catch((err) => console.error(err));
     
   };
+
+
+  function internQuestions() {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'internName',
+        message: "What is the intern's name?",
+      },
+      {
+        type: 'input',
+        name: 'internID',
+        message: "What is the intern's ID number?",
+      },
+      {
+        type: 'input',
+        name: 'internEmail',
+        message: "What is the intern's email address?",
+      },
+      {
+        type: 'input',
+        name: 'internSchool',
+        message: "What is the intern's school?",
+      },
+      {
+        type: 'list',
+        name: "addTeamMember",
+        message: "Select team members to add",
+        choices: ["Engineer", "Intern", "Exit"],
+      }
+     
+    ])
+
+    .then(answers => {
+      let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+      teamMembers.push(intern);
+      if (answers.addTeamMember === "Intern") {
+          internQuestions();
+      } if (answers.addTeamMember === "Engineer") {
+          EngineerQuestions();
+      } if (answers.addTeamMember === "Exit") {
+        generateHTML();
+      }
+    })
+
+    .catch((err) => console.error(err));
+    
+  };
+
+
+  function EngineerQuestions() {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'engineerName',
+        message: "What is the engineer's name?",
+      },
+      {
+        type: 'input',
+        name: 'engineerID',
+        message: "What is the engineer's ID number?",
+      },
+      {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: 'input',
+        name: 'engineerGithub',
+        message: "What is the engineer's github username?",
+      },
+      {
+        type: 'list',
+        name: "addTeamMember",
+        message: "Select team members to add",
+        choices: ["Engineer", "Intern", "Exit"],
+      }
+     
+    ])
+
+    .then(answers => {
+      let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+      teamMembers.push(engineer);
+      if (answers.addTeamMember === "Intern") {
+          internQuestions();
+      } if (answers.addTeamMember === "Engineer") {
+          EngineerQuestions();
+      } if (answers.addTeamMember === "Exit") {
+        generateHTML();
+      }
+    })
+
+    .catch((err) => console.error(err));
+    
+  };
+
+  managerQuestions();
+  function generateHTML() {
+    console.log(teamMembers);
+  }
