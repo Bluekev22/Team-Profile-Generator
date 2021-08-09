33be17df2aa78e9f39aee717dce5e4b728cf4810
Kevin Shank <kevin_shank@yahoo.com>
2021-08-09 14:20:19 -0400

    added initial html

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const util = require('util');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
+const generateHTML = require('./src/generateHTML');
 
 const generatedHTMLPath = "./dist/TeamProfile.html";
 
-let teamMembers = [];
+let teamManager = [];
+let teamEngineers = [];
+let teamInterns = [];
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -43,7 +46,7 @@ function managerQuestions() {
     ])
     .then(answers => {
       let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
-      teamMembers.push(manager);
+      teamManager.push(manager);
       if (answers.addTeamMember === "Intern") {
           internQuestions();
       } if (answers.addTeamMember === "Engineer") {
@@ -89,7 +92,7 @@ function managerQuestions() {
 
     .then(answers => {
       let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
-      teamMembers.push(intern);
+      teamInterns.push(intern);
       if (answers.addTeamMember === "Intern") {
           internQuestions();
       } if (answers.addTeamMember === "Engineer") {
@@ -137,13 +140,13 @@ function managerQuestions() {
 
     .then(answers => {
       let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
-      teamMembers.push(engineer);
+      teamEngineers.push(engineer);
       if (answers.addTeamMember === "Intern") {
           internQuestions();
       } if (answers.addTeamMember === "Engineer") {
           EngineerQuestions();
       } if (answers.addTeamMember === "Exit") {
-        generateHTML();
+        beginHTML();
       }
     })
 
@@ -151,7 +154,48 @@ function managerQuestions() {
     
   };
 
-  managerQuestions();
-  function generateHTML() {
-    console.log(teamMembers);
+  function beginHTML() {
+    writeFileAsync('./dist/index.html', generateHTML());
+    fs.appendFile('./dist/index.html', generateManagerHTML(teamManager), function(err){
+      if (err) throw err;
+    });
+    fs.appendFile('./dist/index.html', generateEngineersHTML(teamEngineers), function(err){
+      if (err) throw err;
+    });
+
   }
+
+  function generateManagerHTML(teamManager) {
+   return `<div class="col-2">
+   <div class="row card">
+       <div class="col-12"><h3>${teamManager[0].name}</h3></div>
+       <div class="col-12"><h4><i class="fas fa-mug-hot"></i>${teamManager[0].getRole()}</h4></div>
+   </div>
+   <div class="row info">
+       <div class="col-12"><p>ID:${teamManager[0].id}</p><p>Email:${teamManager[0].email}</p><p>Office:${teamManager[0].officeNumber}</p></div>
+   </div>
+</div>`
+  }
+  
+  function generateEngineersHTML(teamEngineers) {
+    return `<div class="col-2">
+    <div class="row card">
+        <div class="col-12"><h3>${teamManager[0].name}</h3></div>
+        <div class="col-12"><h4><i class="fas fa-mug-hot"></i>${teamManager[0].getRole()}</h4></div>
+    </div>
+    <div class="row info">
+        <div class="col-12"><p>ID:${teamManager[0].id}</p><p>Email:${teamManager[0].email}</p><p>Office:${teamManager[0].officeNumber}</p></div>
+    </div>
+ </div>`
+   }
+
+  function endOfHTMLPage() {
+    return `</div>
+    </main>
+</body>
+</html>`
+  }
+
+
+
+  managerQuestions();
